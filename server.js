08bcc13a8ce2bd094788369e23ac81d8fa7b71d0
Mike <mabramowski3@gmail.com>
2016-05-15 10:15:49 -0400

    Hello World!

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var server = http.Server(app);
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'html');
+require('./app/routes.js')(app);
+server.listen(port, function() {
+  console.log('listening on *:3000');
+});
+
+module.exports = app;
